79d6cd2733541728b36c06f72bc16160a5b3d80d
sboucher <sboucher2011@gmail.com>
2022-04-08 11:04:28 -0400

    initial commit and firebase setup

---
+++ src/firebase/config.js
@@ -0,0 +1,24 @@
+import firebase from "firebase";
+import 'firebase/.firestore';
+import 'firebase/auth';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAOu8UEsx6kVj9TFgw9_8xEhtnTodEby0w",
+    authDomain: "teamproject-eab77.firebaseapp.com",
+    projectId: "teamproject-eab77",
+    storageBucket: "teamproject-eab77.appspot.com",
+    messagingSenderId: "832030859725",
+    appId: "1:832030859725:web:a1160f71302c3b90036484"
+  };
+
+  //init firebase
+  firebase.initializeApp(firebaseConfig);
+
+  //int services
+  const projectFirestore = firebase.firestore();
+  const projectAuth = firebase.auth();
+
+  // timestamp
+  const timestamp = firebase.firestore.Timestamp;
+
+  export {projectFirestore, projectAuth, timestamp}
